edd6e3014a727ee00d2c414ff18e518190e83e29
LuckMax <106429671+fomm1802@users.noreply.github.com>
2025-10-16 13:01:08 +0700

    Update code.lua

--- code.lua
+++ code.lua
@@ -28,29 +28,26 @@ local SETTINGS = {
 
 --==[ HOP หลังเก็บครบ ]==--
 local HOP = {
-	local Player = game.Players.LocalPlayer    
-local Http = game:GetService("HttpService")
-local TPS = game:GetService("TeleportService")
-local Api = "https://games.roblox.com/v1/games/"
+local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
 
-local _place,_id = game.PlaceId, game.JobId
--- Asc for lowest player count, Desc for highest player count
-local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=10"
-function ListServers(cursor)
-   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-   return Http:JSONDecode(Raw)
+local Servers = "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
+local Server, Next = nil, nil
+local function ListServers(cursor)
+    local Raw = game:HttpGet(Servers .. ((cursor and "&cursor=" .. cursor) or ""))
+    return HttpService:JSONDecode(Raw)
 end
 
-time_to_wait = 120 --seconds
+repeat
+    local Servers = ListServers(Next)
+    Server = Servers.data[math.random(1, (#Servers.data / 3))]
+    Next = Servers.nextPageCursor
+until Server
 
--- choose a random server and join every 2 minutes
-while wait(time_to_wait) do
-   --freeze player before teleporting to prevent synapse crash?
-   Player.Character.HumanoidRootPart.Anchored = true
-   local Servers = ListServers()
-   local Server = Servers.data[math.random(1,#Servers.data)]
-   TPS:TeleportToPlaceInstance(_place, Server.id, Player)
+if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+    TeleportService:TeleportToPlaceInstance(game.PlaceId, Server.id, game.Players.LocalPlayer)
 end
+
 }
 --=========================
 
